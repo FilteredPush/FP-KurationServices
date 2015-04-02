c16f2cc7ea3cf8e0a9e9f42228202291b1bf675e
Paul J. Morris <mole@morris.net>
2015-04-02 03:37:14 +0000

    ISSUE: PURPOSE: Improve matching on botanical author names.  DESCRIPTION:  Adding a small set of standard abbreviations that differ from the name they abbreviate (extending the DC.=de Candolle case).

--- src/main/java/edu/harvard/mcz/nametools/ICNafpAuthorNameComparator.java
+++ src/main/java/edu/harvard/mcz/nametools/ICNafpAuthorNameComparator.java
@@ -19,6 +19,7 @@ package edu.harvard.mcz.nametools;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -339,15 +340,53 @@ public class ICNafpAuthorNameComparator extends AuthorNameComparator {
 		return result;
 	}
      
+    /**
+     * Are anAuthor and anotherAuthor a known match for a small set of 
+     * unusual standard abbreviations linked to name and initials 
+     * (e.g. DC. for de Candolle). 
+     * 
+     * @param anAuthor
+     * @param anotherAuthor
+     * @return true if anAuthor and anotherAuthor form a known pair of
+     * name and abbreviation.
+     */
  	public static boolean knownMatch(String anAuthor, String anotherAuthor) {
 		boolean result = false;
-		if (  (anAuthor.equals("DC") && anotherAuthor.equals("de Candolle")) ||
-			  (anotherAuthor.equals("DC") && anAuthor.equals("de Candolle"))
-		   ) { 
+		
+		HashMap<String,String> knowns = new HashMap<String,String>();
+		knowns.put("DC", "de Candolle");
+		knowns.put("Mendoza-García", "M Mendoza G");
+		knowns.put("L f", "C Linnaeus f");
+		knowns.put("Müll Arg", "J. Müller Arg");
+		knowns.put("Ruiz","H Ruíz L");
+		knowns.put("Germ","J N E Germain S-P");
+		knowns.put("Müll Berol","K Müller Berol");
+		knowns.put("Chick","J W Chickering, Jr");
+		knowns.put("Velez-Nauer","C Vélez N");
+		knowns.put("Ibarra-Manr","G Ibarra M");
+		knowns.put("Vera-Caletti","P Vera C");
+		knowns.put("Cházaro","M J Chazaro B");
+		knowns.put("Sahagun","E Sahagún G");
+		knowns.put("Marrero Rodr","A Marrero R");
+		knowns.put("Magalh","C T Magalhães G");
+		knowns.put("Lucas Rodr","R L Rodriguez C");
+		knowns.put("M Schultz","Schultz, M");
+		knowns.put("Hechav","L Hechavarría S");
+		knowns.put("FrancGut","J A Francisco Gut");
+		knowns.put("Karnk","A Karnkowska-Ish");
+		knowns.put("Sm","J E Smith");
+		knowns.put("Hue","A-M Hue");
+		knowns.put("Day","M A Day");
+		knowns.put("Fr","E M Fries");
+		
+		String m1 = knowns.get(anAuthor.replace(".", ""));
+		String m2 = knowns.get(anotherAuthor.replace(".", ""));
+		
+		if ((m1!=null && m1.equals(anotherAuthor)) || (m2!=null && m2.equals(anAuthor))) { 
 			result = true;
 		}
 		return result;
-	}     
+	}    
  	
  	/**
 	 * If initials are present in both strings, true false if they are different, otherwise
