4ec014e56fe57731a7de412de8f2582692bcc389
swanskysong <swanskysong@9e7fac7f-3ebd-4cb7-9aa0-261363cd0aaf>
2015-05-17 17:48:21 +0000

    Added unified labels for records

--- src/main/java/org/filteredpush/kuration/util/SpecimenRecord.java
+++ src/main/java/org/filteredpush/kuration/util/SpecimenRecord.java
@@ -15,9 +15,24 @@ public class SpecimenRecord extends HashMap<String,String> {
     public static final String SciName_Comment_Label = "scinComment";
     public static final String SciName_Status_Label = "scinStatus";
     public static final String SciName_Source_Label = "scinSource";
+    public static final String date_Comment_Label = "dateComment";
+    public static final String date_Status_Label = "dateStatus";
+    public static final String date_Source_Label = "dateSource";
+    public static final String geoRef_Comment_Label = "geoRefComment";
+    public static final String geoRef_Status_Label = "geoRefStatus";
+    public static final String geoRef_Source_Label = "geoRefSource";
 
     public static final String Original_SciName_Label = "originalScientificName";
     public static final String Original_Authorship_Label = "origialScientificNameAuthorship";
+    public static final String Original_EventDate_Label = "origialEventDate";
+    public static final String Original_Latitude_Label = "origialDecimalLatitude";
+    public static final String Original_Longitude_Label = "origialDecimalLongitude";
+
+    public static final String dwc_scientificName = "scientificName";
+    public static final String dwc_scientificNameAuthorship = "scientificNameAuthorship";
+    public static final String dwc_eventDate = "eventDate";
+    public static final String dwc_decimalLatitude = "decimalLatitude";
+    public static final String dwc_decimalLongitude = "decimalLongitude";
 
     public SpecimenRecord() {
         super();
