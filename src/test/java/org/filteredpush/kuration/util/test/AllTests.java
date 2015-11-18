917d27f6faff45aecdf58cb0593738357fe46f4c
Paul J. Morris <mole@morris.net>
2015-11-18 17:17:27 +0000

    ISSUE:   PURPOSE: Fixing georeference validation errors found by Bob.  DESCRIPTION: Reordering logic in GeoLocate3 validation method so that checks for latitude/longitude out of bounds are performed before invoking geolocate's service.  Fixing error in haversine distance calculation where part of the calculation was performed using degrees and part with radians, when all are supposed to be in radians.  Adding a unit test for this method which covers this case by providing a pair of points neither of which are on either the equator or the greenwich meridian.  Also adding a first cut at a skeletal data structure for degrees with specified precision.

---
+++ src/test/java/org/filteredpush/kuration/util/test/AllTests.java
@@ -0,0 +1,14 @@
+package org.filteredpush.kuration.util.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	DateUtilsTest.class, 
+	GeoUtiltsTest.class
+	})
+public class AllTests {
+
+}
